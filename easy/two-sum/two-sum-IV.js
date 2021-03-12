bcaac5af5d5278aee5685bf6346a3f60724fdb3b
taiyue <taiyue@hanlin.com.tw>
2021-03-12 18:06:36 +0800

    add new question

--- easy/two-sum/two-sum-IV.js
+++ easy/two-sum/two-sum-IV.js
@@ -17,6 +17,9 @@ let find = function(root, needNum) {
 }
 
 let findTarget = function(root, k) {
+  if (root === null)
+    return false
+
   let needNum = k - root.val
   // let rs = find(root, needNum)
   // if (rs === true)
@@ -28,17 +31,22 @@ let findTarget = function(root, k) {
   else if (root.right !== null)
     rs = find(root.right, k)
 
-  
+  if (rs === true)
+    return true
+  else if (needNum < root.val)
+    return findTarget(root.left, k)
+  else if (needNum > root.val)
+    return findTarget(root.right, k)
 
   return false
 }
 
-// let root1 = new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6, null, new TreeNode(7)))
-// console.log(findTarget(root1, 9))    // true
-// console.log(findTarget(root1, 28))   // false
-// let root2 = new TreeNode(2, new TreeNode(1), new TreeNode(3))
-// console.log(findTarget(root2, 4))                   // true
-// console.log(findTarget(root2, 1))                   // false
-// console.log(findTarget(root2, 3))                   // true
-// console.log(findTarget(new TreeNode(1), 2))                   // false
+let root1 = new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6, null, new TreeNode(7)))
+console.log(findTarget(root1, 9))    // true
+console.log(findTarget(root1, 28))   // false
+let root2 = new TreeNode(2, new TreeNode(1), new TreeNode(3))
+console.log(findTarget(root2, 4))                   // true
+console.log(findTarget(root2, 1))                   // false
+console.log(findTarget(root2, 3))                   // true
+console.log(findTarget(new TreeNode(1), 2))                   // false
 console.log(findTarget(new TreeNode(3, new TreeNode(1)), 2))                   // false
